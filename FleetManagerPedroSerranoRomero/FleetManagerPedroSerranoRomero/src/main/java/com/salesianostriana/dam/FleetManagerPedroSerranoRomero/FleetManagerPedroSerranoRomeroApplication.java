4c18c582054ef1e84d293ba73f98c00d023da5bf
PedritoSerrano <serrano.roped24@triana.salesianos.edu>
2026-01-14 09:49:18 +0100

    models

---
+++ FleetManagerPedroSerranoRomero/FleetManagerPedroSerranoRomero/src/main/java/com/salesianostriana/dam/FleetManagerPedroSerranoRomero/FleetManagerPedroSerranoRomeroApplication.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.FleetManagerPedroSerranoRomero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleetManagerPedroSerranoRomeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FleetManagerPedroSerranoRomeroApplication.class, args);
+	}
+
+}
