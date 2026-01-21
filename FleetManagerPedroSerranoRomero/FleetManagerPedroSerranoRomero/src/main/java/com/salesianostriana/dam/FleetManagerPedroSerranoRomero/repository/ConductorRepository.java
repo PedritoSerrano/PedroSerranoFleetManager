82a9caf1dd28d4b13abbfa10997642c5bce3afac
PedritoSerrano <serrano.roped24@triana.salesianos.edu>
2026-01-21 10:01:17 +0100

    specifications

--- FleetManagerPedroSerranoRomero/FleetManagerPedroSerranoRomero/src/main/java/com/salesianostriana/dam/FleetManagerPedroSerranoRomero/repository/ConductorRepository.java
+++ FleetManagerPedroSerranoRomero/FleetManagerPedroSerranoRomero/src/main/java/com/salesianostriana/dam/FleetManagerPedroSerranoRomero/repository/ConductorRepository.java
@@ -2,6 +2,13 @@ package com.salesianostriana.dam.FleetManagerPedroSerranoRomero.repository;
 
 import com.salesianostriana.dam.FleetManagerPedroSerranoRomero.model.Conductor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ConductorRepository extends JpaRepository<Conductor, Long> {
+
+//    @Query
+
+
 }
--- FleetManagerPedroSerranoRomero/FleetManagerPedroSerranoRomero/src/main/java/com/salesianostriana/dam/FleetManagerPedroSerranoRomero/services/ConductorService.java
+++ FleetManagerPedroSerranoRomero/FleetManagerPedroSerranoRomero/src/main/java/com/salesianostriana/dam/FleetManagerPedroSerranoRomero/services/ConductorService.java
@@ -1,4 +1,30 @@
 package com.salesianostriana.dam.FleetManagerPedroSerranoRomero.services;
 
+import com.salesianostriana.dam.FleetManagerPedroSerranoRomero.model.Conductor;
+import com.salesianostriana.dam.FleetManagerPedroSerranoRomero.repository.ConductorRepository;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ConductorService {
+
+    private final ConductorRepository conductorRepository;
+
+    public ConductorService(ConductorRepository conductorRepository) {
+        this.conductorRepository = conductorRepository;
+    }
+
+    List<Conductor> filtrar (String nombre, String email) {
+        return conductorRepository.findAll();
+    }
+
+    private static class ConductorSpecs {
+
+        static Specification<Conductor> nombreContains (String nombre) {
+            return ((root, query, criteriaBuilder) ->
+                    nombre == null ? null : criteriaBuilder.like(criteriaBuilder.lower(root.get("nombre")), "%" + nombre.toLowerCase() + "%"));
+        }
+    }
 }
